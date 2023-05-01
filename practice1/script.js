f93271b2089cfc5e2f04d18814bef9e81787a140
bhabesh2 <jsarma058@gmail.com>
2023-05-01 19:45:50 +0530

    edit done

--- practice1/script.js
+++ practice1/script.js
@@ -1,18 +1,15 @@
 let time = document.getElementsByClassName("time")[0];
-let morOrevg = document.getElementsByClassName("morOrEvg")[0];
 
 setInterval(() => {
     let date = new Date();
     let h = date.getHours() % 12 || 12;
     let amOrPm = date.getHours() >= 12 ? 'PM' : 'AM';
-    let morOrevg = date.getHours() >= 12 ? 'Good Afternoon' : 'Good Morning';
-    if (h >= 12 && amOrPm === 'PM' && morOrevg === "Good Afternoon") {
+    if (h >= 12 && amOrPm === 'PM') {
         h -= 12;
     }
     let min = date.getMinutes().toString().padStart(2, '0');
     let sec = date.getSeconds().toString().padStart(2, '0');
     let formattedH = h.toString().padStart(2, '0');
-    time.innerHTML = `${formattedH}:${min}:${sec} ${amOrPm}`;
-    morOrevg.innerHTML = `${morOrevg}`
+    time.innerHTML = `${formattedH}:${min}:${sec} ${amOrPm}`
 }, 1000);
 
